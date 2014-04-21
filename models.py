c85218800dae5d45e74d6402295ff1292352878c
Wayne <guowangyang@gmail.com>
2014-04-21 17:31:51 +0800

    + models.py

---
+++ models.py
@@ -0,0 +1,25 @@
+from peewee import *
+
+
+DB_NAME = "db.sqlite"
+database = SqliteDatabase(DB_NAME)
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Local(BaseModel):
+    title = CharField()
+    path = CharField()
+    md5 = CharField()  # MD5 checksum of file
+    last_modified = DateField()
+
+
+class Flickr(BaseModel):
+    photoid = CharField(unique=True)
+    local = ForeignKeyField(Local, null=True)  # mapping to local file
+    photoset = CharField(null=True)  # if photo belong to certain set
+    lastupdate = CharFiled()
+    ispublic = BooleanField()
